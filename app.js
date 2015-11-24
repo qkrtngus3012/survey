9272b2fbcb2dab753c594d86aaa89abf4c709ea3
qkrtngus3012 <qkrtngus3012@hanmail.net.com>
2015-11-24 14:34:02 +0900

    commit for test

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
@@ -20,6 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'secret long password for session 2015-11'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
