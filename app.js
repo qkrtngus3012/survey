23d68a5a7e41efc2a4cd3690163df716af800920
qkrtngus3012 <qkrtngus3012@hanmail.net.com>
2015-11-28 13:56:47 +0900

    change the app.js add post

--- app.js
+++ app.js
@@ -4,15 +4,27 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var posts = require('./routes/posts');
+var mongoose   = require('mongoose');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+app.locals.moment = require('moment');
+
+// mongodb connect
+mongoose.connect('mongodb://suhyun:tn462513@ds051334.mongolab.com:51334/suhyun');
+mongoose.connection.on('error', console.log);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,9 +33,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
