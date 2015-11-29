62c29fb4fb1e9751396509d6b3e8acd7238ff313
qkrtngus3012 <qkrtngus3012@hanmail.net.com>
2015-11-29 21:01:01 +0900

    change the app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
--
-+var session = require('express-session');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-+
 var app = express();
 
 // view engine setup
@@ -19,11 +20,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-+app.use(session({
-+  resave: true,
-+  saveUninitialized: true,
-+  secret: 'secret long password for session 2015-11'
-+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
@@ -47,3 +43,18 @@ if (app.get('env') === 'development') {
       message: err.message,
       error: err
     });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
