86eaf135ccdae722198503889518a747e57fddd2
qkrtngus3012 <qkrtngus3012@hanmail.net.com>
2015-11-29 20:50:31 +0900

    back

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-
-
+-
++var session = require('express-session');
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var posts = require('./routes/posts');
-var mongoose   = require('mongoose');
 
++
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-if (app.get('env') === 'development') {
-  app.locals.pretty = true;
-}
-app.locals.moment = require('moment');
-
-// mongodb connect
-mongoose.connect('mongodb://suhyun:tn462513@ds051334.mongolab.com:51334/suhyun');
-mongoose.connection.on('error', console.log);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -32,13 +19,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
++app.use(session({
++  resave: true,
++  saveUninitialized: true,
++  secret: 'secret long password for session 2015-11'
++}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(path.join(__dirname, '/bower_components')));
-app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,18 +47,3 @@ if (app.get('env') === 'development') {
       message: err.message,
       error: err
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
